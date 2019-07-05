96c48193cf7e015d1bcf6c64db42afeb87a7f80e
iurylemos <iurylemos10@gmail.com>
2019-07-05 14:29:13 -0300

    Obtendo os dados do firebase para colocar na tela inicial

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     /**Função que retorna um degrade, que é uma imagem antes da pag antes
      * de ser carregada, ou seja ela é um back antes do que vai ter em cima
      * que é o stack
      * Retorna um container
      */
     Widget _buidBodyBack() => Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          //Gradiente vai ter duas cores, uma forte em cima e mais fraco em baixo
-            colors: [
-              Color.fromARGB(255, 211, 118, 130),
-              Color.fromARGB(255, 253, 181, 168)
-            ],
-          //Dizendo o fim do gradiente
-          begin: Alignment.topRight,
-          end: Alignment.bottomRight
-        )
-      ),
-    );
-
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  //Gradiente vai ter duas cores, uma forte em cima e mais fraco em baixo
+                  colors: [
+                Color.fromARGB(255, 211, 118, 130),
+                Color.fromARGB(255, 253, 181, 168)
+              ],
+                  //Dizendo o fim do gradiente
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomRight)),
+        );
 
     //Stack pois quero colocar um conteudo acima do meu fundo(DEGRADE)
     return Stack(
@@ -50,6 +48,44 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
+            ),
+            /**Botendo os dados do firebase, como ele não é instantaniamente
+             * Utilizo o FutureBuilder, pois vou mostrar na tela que está carregando.
+             */
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .getDocuments(),
+              /** builder é a função que vai criar o que vai ter na tela
+               * dependendo do que o meu future trazer.
+               */
+              builder: (context, snapshot) {
+                //Se o snapshot não possui dado
+                if(!snapshot.hasData) {
+                  //Como estou dentro do Sliver não posso colocar simplesmente o CircleProssessIndicator
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        //Vou botar a cor branca nele
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                  //Caso eu tenha algum DADO
+                } else {
+                  print(snapshot.data.documents.length);
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: Container()
+                    ),
+                  );
+                }
+              },
             )
           ],
         )
